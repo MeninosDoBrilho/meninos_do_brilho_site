70a4d4a8118b83312519c7c90e0b264d822471af
Lauro Mendes do Amaral Junior <lauro.juniorspx@gmail.com>
2020-07-26 00:53:00 -0700

    testes para mobile

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+let show = true;
+
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = document.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => { 
+    menuSection.classList.toggle("on", show)
+
+    document.body.style.overflow = show ? "hidden" : "initial" 
+
+    show = !show;
+})
