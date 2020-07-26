9f2e3bad420fd2fa5adc66e7ad9576bc1906f86f
Lauro e Daniel <meninos.do.brilho@gmail.com>
2020-07-26 20:44:55 -0300

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,6 @@
-let show = true;
-
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = document.querySelector(".menu-toggle")
 
 menuToggle.addEventListener("click", () => { 
-    menuSection.classList.toggle("on", show)
-
-    document.body.style.overflow = show ? "hidden" : "initial" 
-
-    show = !show;
+    menuSection.classList.toggle("on")
 })
